eb12138a63dbea32fbf279aaf1ead3ef9a4dc307
Auguste Janickaite <auguste@Augustes-MacBook-Pro.local>
2022-10-12 21:36:25 +0300

    updated navbar for mobile with burger menu

--- index-script.js
+++ index-script.js
@@ -1,2 +1,17 @@
+function mobileNavTransformation() {
+    var mobileNavItems = document.querySelectorAll('.web-mob, .mobile, .nav-ul');
+    console.log(mobileNavItems);
 
-console.log("prisegta")
+    mobileNavItems.forEach( function (item) {
+        console.log(item);
+
+        if (item.classList.contains('responsive')) {
+            console.log('treu');
+            item.classList.remove('responsive');
+
+        } else {
+            item.classList.add('responsive');
+
+        }
+    })
+}
