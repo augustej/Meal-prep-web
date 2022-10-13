4014eed8adb7341f551ea792ccde06d2f19baee2
Auguste Janickaite <auguste@Augustes-MacBook-Pro.local>
2022-10-13 09:19:53 +0300

    delated console.logs

--- index-script.js
+++ index-script.js
@@ -1,12 +1,9 @@
 function mobileNavTransformation() {
     var mobileNavItems = document.querySelectorAll('.web-mob, .mobile, .nav-ul');
-    console.log(mobileNavItems);
 
     mobileNavItems.forEach( function (item) {
-        console.log(item);
 
         if (item.classList.contains('responsive')) {
-            console.log('treu');
             item.classList.remove('responsive');
 
         } else {
