1c4af8d52e7c24ac3c2a0b2075f2c0d95ebe1c24
Auguste Janickaite <auguste@Augustes-MacBook-Pro.local>
2022-10-19 20:51:18 +0300

    login, signup, logout implemented

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,16 @@
 # from symbol import import_as_name
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DATABASE_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Su>{PeRSECRETmYWeb?site180some'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_NAME}'
+    db.init_app(app)
 
     from .public_pages import public_pages
     from .private_pages import private_pages
@@ -13,5 +20,18 @@ def create_app():
     app.register_blueprint(private_pages, url_prefix = '/')
     app.register_blueprint(auth, url_prefix = '/')
 
+    from .model import User
+
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+        
     return app
 
--- flaskr/public_pages.py
+++ flaskr/public_pages.py
@@ -1,10 +1,16 @@
 from flask import Blueprint, render_template, redirect
+from flask_login import current_user
+from .model import User
 
 public_pages = Blueprint('public_pages', __name__)
 
 @public_pages.route('/')
 def home():
-    return render_template('pages/public/index.html')
+    if current_user.is_authenticated:
+        name = current_user.name
+        return render_template('pages/private/personal_home.html', name=name)
+    else:
+        return render_template('pages/public/index.html')
 
 @public_pages.route('/recipes')
 def recipes():
