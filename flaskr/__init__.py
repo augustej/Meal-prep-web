1ed27d95919354b4a8ae67f5cc258ac27c94e272
Auguste Janickaite <auguste@Augustes-MacBook-Pro.local>
2022-11-16 15:37:44 +0200

    created send email functionality

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,17 +3,27 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from sqlalchemy import inspect, create_engine
+from flask_mail import Mail
 
 db = SQLAlchemy()
 DATABASE_NAME = "database.db"
 UPLOAD_FOLDER = '/Users/auguste/Desktop/git/Meal-prep-web/flaskr/static/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Su>{PeRSECRETmYWeb?site180some'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DATABASE_NAME}'
     app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    # app.config['EMAIL_PORT'] = 7777
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_USERNAME'] = 'savaites.meniu.planas@gmail.com'
+    app.config['MAIL_PASSWORD'] = 'mcwdzlttcfluwusm'
+    mail.init_app(app)
+
     db.init_app(app)
 
     from .public_pages import public_pages
